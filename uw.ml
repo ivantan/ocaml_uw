4272432f4a84998efe4d747ea75ef7f2a72213f7
ivantan <ivan.wjt@gmail.com>
2015-05-06 03:24:39 +0800

    intro and basic to OCaml

---
+++ uw.ml
@@ -0,0 +1,7 @@
+(* our first program *)
+(* don't forget the jj
+   and loading modules require a hash "#load"
+   and the difference between local and global expressions
+   expressions are not really variables, to use variables use
+   references, as in "let my_ref = ref 0;;" 
+*)
